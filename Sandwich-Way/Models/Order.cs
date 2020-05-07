362ce87da6d06616243862c5dc0631c4a7790c1e
Anthony Simon <anthony.simon2020@outlook.com>
2020-05-07 10:35:44 -0700

    Input Validation

--- Sandwich-Way/Models/Order.cs
+++ Sandwich-Way/Models/Order.cs
@@ -1,24 +1,61 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Sandwich_Way.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
-        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Please enter your first name")]
+        [Display(Name = "First Name")]
+        [StringLength(25)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your street address")]
+        [Display(Name = "Street Address")]
+        [StringLength(100)]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [Display(Name = "City")]
+        [StringLength(50)]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your state")]
+        [Display(Name = "State")]
+        [StringLength(2,MinimumLength = 2)]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Zip Code")]
+        [Display(Name = "Zip Code")]
+        [StringLength(12, MinimumLength = 8)]
         public string ZipCode { get; set; }
+
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
         public List <OrderDetail> OrderDetails { get; set; }
 
+        [BindNever]
         public decimal OrderTotal { get; set; }
+        
+        [BindNever]
         public DateTime OrderPlaced { get; set; }
     }
 }
